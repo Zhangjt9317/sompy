c9a6fa7b0b5ce5869fa9331283f5486f942484bf
ldocao <ldocao@quantmetry.com>
2016-07-04 11:25:46 +0200

    refactor: move visualisation into sompy

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
 setup(
-    name="SOMPY",
-    version="1.0",
+    name="sompy",
+    version="2.0",
     description="Self Organizing Maps Package",
     author="",
-    packages=["SOMPY","visualization"]
+    packages=["SOMPY"]
 )

30c015459a2e19a8d84b1d1aa19fdc257acfdb83
ldocao <ldocao@quantmetry.com>
2016-07-04 11:35:07 +0200

    content: add example from original notebook

---
+++ example/som.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from time import time
+import sys
+import sompy
+from visualization import mapview
+
+### A toy example: two dimensional data, four clusters
+
+dlen = 200
+Data1 = pd.DataFrame(data= 1*np.random.rand(dlen,2))
+Data1.values[:,1] = (Data1.values[:,0][:,np.newaxis] + .42*np.random.rand(dlen,1))[:,0]
+
+
+Data2 = pd.DataFrame(data= 1*np.random.rand(dlen,2)+1)
+Data2.values[:,1] = (-1*Data2.values[:,0][:,np.newaxis] + .62*np.random.rand(dlen,1))[:,0]
+
+Data3 = pd.DataFrame(data= 1*np.random.rand(dlen,2)+2)
+Data3.values[:,1] = (.5*Data3.values[:,0][:,np.newaxis] + 1*np.random.rand(dlen,1))[:,0]
+
+
+Data4 = pd.DataFrame(data= 1*np.random.rand(dlen,2)+3.5)
+Data4.values[:,1] = (-.1*Data4.values[:,0][:,np.newaxis] + .5*np.random.rand(dlen,1))[:,0]
+
+
+Data1 = np.concatenate((Data1,Data2,Data3,Data4))
+
+fig = plt.figure()
+plt.plot(Data1[:,0],Data1[:,1],'ob',alpha=0.2, markersize=4)
+fig.set_size_inches(7,7)
+
+mapsize = [20,20]
+som = sompy.SOMFactory.build(Data1, mapsize, mask=None, mapshape='planar', lattice='rect', normalization='var', initialization='pca', neighborhood='gaussian', training='batch', name='sompy')  # this will use the default parameters, but i can change the initialization and neighborhood methods
+som.train(n_job=1, verbose='info')
+
+
+v = mapview.View2DPacked(50, 50, 'test', text_size=8)
+v.show(som, what='codebook', which_dim=[0,1], CMAP=None, col_sz=6)
+plt.show()
